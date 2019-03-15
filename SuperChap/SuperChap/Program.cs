fba43571227139104f4c03608fda43018bf0c08e
Casper Naudts <casper@edu-tech.be>
2019-03-15 12:06:57 +0100

    init project in VisualStudio

---
+++ SuperChap/SuperChap/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SuperChap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SuperChap/SuperChat.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SuperChat.Data
+{
+    public class Class1
+    {
+    }
+}
