3647a92291763ff8b84023479492d51d852a3b0b
Yonas <yonas.dresen@outlook.com>
2019-03-21 20:32:30 +0100

    - Added EF Core NuGet Packages - Added Domain classes (User, Message, Chat) - Added DbContext

---
+++ SuperChat/SuperChat.Data/SuperChatContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperChat.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperChat.Data
+{
+    class SuperChatContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = SuperChatAppData; Trusted_Connection = True; ");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ SuperChat/SuperChat.Domain/Chat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperChat.Domain
+{
+    public class Chat
+    {
+
+        public IList<User> UserIds { get; set; } = new List<User>();
+        public IList<Message> Messages { get; set; } = new List<Message>();
+
+    }
+}
