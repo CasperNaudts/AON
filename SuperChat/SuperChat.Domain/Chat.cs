87cea3f80b588fac05e9254c895bf948cdf475cd
Yonas Dresen <yonas.dresen@outlook.com>
2019-03-22 11:57:39 +0100

    classes

--- SuperChat/SuperChat.Domain/Chat.cs
+++ SuperChat/SuperChat.Domain/Chat.cs
@@ -6,8 +6,7 @@ namespace SuperChat.Domain
 {
     public class Chat
     {
-
-        public IList<User> UserIds { get; set; } = new List<User>();
+        public IList<User> Users { get; set; } = new List<User>();
         public IList<Message> Messages { get; set; } = new List<Message>();
 
     }
