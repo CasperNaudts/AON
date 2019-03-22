42480e08d2274d398aab0fd01f98e4f874bfb838
Brecht Van Camp <11701504@student.pxl.be>
2019-03-22 12:13:30 +0100

    create UI Console project

---
+++ SuperChat/SuperChat.UI.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SuperChat.UI.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
