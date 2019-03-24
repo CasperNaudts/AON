62d2d7378685019e3e77a42029f3ac48e1022f36
Casper Naudts <casper@edu-tech.be>
2019-03-24 17:25:13 +0100

    added first migration

---
+++ SuperChat/MigrationStartup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MigrationStartup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
