e678b3f085398c04adcde5588b6069dacd972ce5
Casper Naudts <casper@edu-tech.be>
2019-03-29 12:24:09 +0100

    start console app

--- SuperChat/SuperChat.UI.Console/Program.cs
+++ SuperChat/SuperChat.UI.Console/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace SuperChat.UI.Console
 {
@@ -6,7 +8,23 @@ namespace SuperChat.UI.Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Welkom");
+            System.Console.WriteLine("Wilt U een bericht encrypteren (E) of decrypteren(D)");
+
+            string choise = System.Console.ReadLine();
+            string[] posibleChoises = {"encrypteren", "E", "e", "decrypteren", "d", "D"};
+            while (!posibleChoises.Contains(choise))
+            {
+                System.Console.WriteLine("Geen geldige keuze.");
+                System.Console.WriteLine("Wilt U een bericht encrypteren (E) of decrypteren(D)");
+
+                choise = System.Console.ReadLine();
+            }
+
+            if (choise == "e" || choise == "E" || choise == "encrypteren")
+            {
+                
+            }
         }
     }
 }
